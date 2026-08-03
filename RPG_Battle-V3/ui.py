ffa0bea60739130630bc3ffda5b01c5dc4811132
S4LMUN <chayutpong2154@gmail.com>
2026-08-03 17:54:45 +0700

    feat(rpg-battle-v3): add main menu UI function

---
+++ RPG_Battle-V3/ui.py
@@ -0,0 +1,11 @@
+# === ui.py === #
+
+# === default variable === #
+
+def main_menu():
+    print()
+    print(" === RPG BATTLE Game ===")
+    print()
+    print("1) PLAY")
+    print("2) EXIT")
+    print()
