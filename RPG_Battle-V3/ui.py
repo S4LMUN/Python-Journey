77675dccdcccff07ccb8777afe58f7497c24b70d
S4LMUN <chayutpong2154@gmail.com>
2026-08-03 17:58:16 +0700

    feat(rpg-battle-v3): add ask_menu UI function

--- RPG_Battle-V3/ui.py
+++ RPG_Battle-V3/ui.py
@@ -9,3 +9,16 @@ def main_menu():
     print("1) PLAY")
     print("2) EXIT")
     print()
+    ask_menu(1,2,"Func")
+
+def ask_menu(low,high,text):
+    try:
+        result = int(input(f" > Select {text} : "))
+        if low <= result <= high:
+            return result
+        else:
+            print("Invalid Value")
+            return None
+    except ValueError:
+        print("Value Error")
+        return None
