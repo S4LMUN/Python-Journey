7d92d72383f4628520953b70faa776e6e02499fe
S4LMUN <chayutpong2154@gmail.com>
2026-08-03 18:21:13 +0700

    feat(rpg-battle-v3): add heall method to Monster class

--- RPG_Battle-V3/monster.py
+++ RPG_Battle-V3/monster.py
@@ -15,3 +15,9 @@ class Monster:
     def attack(self):
         damage = random.randint(3,8)
         return damage
+
+    def heal(self):
+        heal = random.randint(1,4)
+        self.hp += heal
+        self.hp = min(self.hp,self.max_hp)
+        return heal
