3cc43c5fda663efaa7237c0f0b03025f7cf16247
S4LMUN <chayutpong2154@gmail.com>
2026-08-04 17:52:44 +0700

    update(rpg-battle-v3): handle main menu choices and exit confirmation

--- RPG_Battle-V3/battle.py
+++ RPG_Battle-V3/battle.py
@@ -2,6 +2,16 @@
 
 # === import === #
 
+from player import Player
+from monster import Monster
 import ui
 
+# === default variable === #
+
+player = Player("David", 100)
+monster = Monster("Skeleton", 80)
+
 # === main func === #
+
+def start():
+    ui.battle(player,monster)
