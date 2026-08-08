e92688a5343ef43a23210ae643082e20a7e44da2
S4LMUN <chayutpong2154@gmail.com>
2026-08-08 09:42:24 +0700

    feat(RPG BATTLE V4): setup main menu control flow

--- RPG_Battle-V4/main.py
+++ RPG_Battle-V4/main.py
@@ -2,6 +2,20 @@
 
 # === import === #
 
+import ui
+
 # === default variable === # 
 
+run = True
+
 # === main func === #
+
+while run:
+    result = ui.main_menu()
+    if result is None:
+        continue
+    else:
+        if result == 1:
+            pass
+        elif result == 2:
+            pass
