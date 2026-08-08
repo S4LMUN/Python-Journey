ac31140f513b408216ac29e303b02a1d947db26e
S4LMUN <chayutpong2154@gmail.com>
2026-08-08 09:42:24 +0700

    feat(RPG BATTLE V4): setup main menu control flow

--- RPG_Battle-V4/ui.py
+++ RPG_Battle-V4/ui.py
@@ -5,3 +5,25 @@
 # === default variable === # 
 
 # === main func === #
+
+def main_menu():
+    print()
+    print(" === RPG BATTLE V4 ===")
+    print()
+    print("1) Battle")
+    print("2) Exit")
+    print()
+    result = ask_menu(1,2,"Choice")
+    return result
+
+def ask_menu(low,high,text):
+    try:
+        result = int(input(f" > Select {text} : "))
+        if low <= result <= high:
+            return result
+        else:
+            print("Invalid Value")
+            return None
+    except ValueError:
+        print("Value Error")
+        return None
