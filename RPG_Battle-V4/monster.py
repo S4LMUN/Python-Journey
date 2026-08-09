89051c383ac337caa7582870d2ad034ab6c7ab91
S4LMUN <chayutpong2154@gmail.com>
2026-08-09 19:18:31 +0700

    feat(RPG BATTLE V4): update attack method to return damage

--- RPG_Battle-V4/monster.py
+++ RPG_Battle-V4/monster.py
@@ -20,9 +20,17 @@ class Monster:
     def attack(self,target):
         damage = random.randint(3,10)
         result = target.take_damage(damage)
-        return result
+        return result, damage
         
     def take_damage(self,damage):
         self.hp -= damage
         self.hp = max(self.hp,0)
         return self.hp > 0
+
+    def heal(self):
+        heal = random.randint(5,9)
+        self.hp += heal
+        self.hp = min(self.hp,self.max_hp)
+
+    def decided(self,target):
+        pass # Do this
