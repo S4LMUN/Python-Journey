fcb1ca0b14fe41dcd38befdeba451532174c4274
S4LMUN <chayutpong2154@gmail.com>
2026-08-12 09:40:16 +0700

    refactor(RPG BATTLE V4): player and monster turn ui in battle loop

--- RPG_Battle-V4/battle.py
+++ RPG_Battle-V4/battle.py
@@ -25,17 +25,17 @@ def start():
         player_action = ui.battle(player,monster)
         if player_action == 1:
             monster_alive,player_damage = player.attack(monster)
-            ui.action()
-            ui.attack(player,player_damage,monster)
+            player_turn(player,player_damage,monster,1)
             if monster_alive is False:
                 break
             else:
-                player_alive, monster_value, action = monster.decided(player)
-                # show monster action
+                player_alive, monster_value, monster_action = monster.decided(player)
+                monster_turn(monster,monster_action,monster_value,player)    
         elif player_action == 2:
             player_heal = player.heal()
-            ui.action()
-            ui.heal(player,player_heal)
+            player_turn(player,player_heal,monster,2)
+            player_alive, monster_value, monster_action = monster.decided(player)
+            monster_turn(monster,monster_action,monster_value,player)
         elif player_action == 3:
             confirm = ui.ask_confirm("run")
             if confirm.lower() == "y":
@@ -44,3 +44,16 @@ def start():
                 print()
                 print(f"PLAYER  | {player.name} RUN OUT OF THE BATTLE")
                 break
+
+def monster_turn(monster,monster_action,monster_value,target):
+    if monster_action == 1:
+        ui.attack(monster,monster_value,target)
+    elif monster_action == 2:
+        ui.heal(monster,monster_value)
+
+def player_turn(player,player_value,target,action):
+    ui.action()
+    if action == 1:
+        ui.attack(player,player_value,target)
+    elif action == 2:
+        ui.heal(player,player_value)
