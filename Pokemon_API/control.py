25d51790154b60781e283e49de36f12dfccde493
S4LMUN <chayutpong2154@gmail.com>
2026-08-26 18:34:08 +0700

    feat(Pokemon_API): Can get pokemon api

--- Pokemon_API/control.py
+++ Pokemon_API/control.py
@@ -1,13 +1,15 @@
 # control.py #
 
 import ui
+import requests
 
 def start():
     run = True
+    url = "https://pokeapi.co/api/v2/pokemon/pikachu"
     while run:
         pokemon = ui.main_menu()
         if pokemon is None:
             continue
         else:
-            pass
-
+            response = requests.get(url)
+            print(response.status_code)
