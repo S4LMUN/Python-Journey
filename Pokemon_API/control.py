9d71b3c61c2f6f5c02aa98b4cb946a411dfcd744
S4LMUN <chayutpong2154@gmail.com>
2026-08-27 17:59:43 +0700

    feat(Pokemon_API): print Pokemon Types

--- Pokemon_API/control.py
+++ Pokemon_API/control.py
@@ -14,13 +14,18 @@ def start():
             url = main_url + pokemon
             response = requests.get(url)
             if response.status_code == 200:
+                type_list = []
                 data = response.json()
                 print()
                 print(f"Name   : {data["name"]}")
                 print(f"ID     : {data["id"]}")
                 print(f"Height : {data["height"]}")
                 print(f"Weight : {data["weight"]}")
+                for type_data in data["types"]:
+                    type_list.append(type_data["type"]["name"])
+                print(f"Types  : {", ".join(type_list)}")
             else:
+                print()
                 print(f"No info about pokemon {pokemon}")
                 continue
 
